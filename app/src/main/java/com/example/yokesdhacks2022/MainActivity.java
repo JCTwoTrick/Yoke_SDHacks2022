0e0e57de66eba34fc979d13e776ce3072ff2f1d9
JCTwoTrick <76675349+JCTwoTrick@users.noreply.github.com>
2022-04-09 16:09:41 -0700

    Added new page

--- app/src/main/java/com/example/yokesdhacks2022/MainActivity.java
+++ app/src/main/java/com/example/yokesdhacks2022/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.yokesdhacks2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.start_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, question1.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/yokesdhacks2022/question1.java
@@ -0,0 +1,15 @@
+package com.example.yokesdhacks2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class question1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question1);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+    }
+}
