6ca5c8516a3bae30d6b0559d671f53c2a7bc9f08
JCTwoTrick <76675349+JCTwoTrick@users.noreply.github.com>
2022-04-09 18:03:10 -0700

    Added new page

--- app/src/main/java/com/example/yokesdhacks2022/question1.java
+++ app/src/main/java/com/example/yokesdhacks2022/question1.java
@@ -2,14 +2,90 @@ package com.example.yokesdhacks2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class question1 extends AppCompatActivity {
 
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question1);
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+        ArrayList goals = new ArrayList();
+
+        final Button maintain_weight = findViewById(R.id.maintain_weight);
+        maintain_weight.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+               if(goals.contains("Maintain Weight")) {
+                   goals.remove("Maintain Weight");
+                   maintain_weight.setBackgroundColor(Color.parseColor("#FF80CBC4"));
+               }
+               else {
+                   goals.add("Maintain Weight");
+                   maintain_weight.setBackgroundColor(Color.parseColor("#FF80B3CB"));
+               }
+            }
+        });
+
+        final Button bulk = findViewById(R.id.bulk);
+        bulk.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                if(goals.contains("Bulk")) {
+                    goals.remove("Bulk");
+                    bulk.setBackgroundColor(Color.parseColor("#FF80CBC4"));
+                }
+                else {
+                    goals.add("Bulk");
+                    bulk.setBackgroundColor(Color.parseColor("#FF80B3CB"));
+                }
+            }
+        });
+
+        final Button lean = findViewById(R.id.lean);
+        lean.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                if(goals.contains("Lean")) {
+                    goals.remove("Lean");
+                    lean.setBackgroundColor(Color.parseColor("#FF80CBC4"));
+                }
+                else {
+                    goals.add("Lean");
+                    lean.setBackgroundColor(Color.parseColor("#FF80B3CB"));
+                }
+            }
+        });
+
+        final Button lose_weight = findViewById(R.id.lose_weight);
+        lose_weight.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                if(goals.contains("Lose Weight")) {
+                    goals.remove("Lose Weight");
+                    lose_weight.setBackgroundColor(Color.parseColor("#FF80CBC4"));
+                }
+                else {
+                    goals.add("Lose Weight");
+                    lose_weight.setBackgroundColor(Color.parseColor("#FF80B3CB"));
+                }
+            }
+        });
+
+        final Button next1 = findViewById(R.id.next1);
+        next1.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                if(goals.size() != 0) {
+
+                }
+            }
+        });
     }
 }
